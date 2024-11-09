4b3c394c2c29e4314d83c66d4182a35fa14a1f5f
inegohe <ikhosler@gmail.com>
2024-11-09 04:32:12 +0300

    start bookmark screen

--- app/(tabs)/bookmark.jsx
+++ app/(tabs)/bookmark.jsx
@@ -1,12 +1,56 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  FlatList,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+import SearchInput from "../../components/SearchInput";
+import EmptyState from "../../components/EmptyState";
+import { searchPosts } from "../../lib/appwrite";
+import useAppwrite from "../../lib/useAppwrite";
+import VideoCard from "../../components/VideoCard";
+import { useLocalSearchParams } from "expo-router";
 
 const Bookmark = () => {
-  return (
-    <View>
-      <Text>Bookmark</Text>
-    </View>
-  )
-}
+  const { query } = useLocalSearchParams();
 
-export default Bookmark
+  const { data: posts, refetch } = useAppwrite(() => searchPosts(query));
+
+  const [refreshing, setRefreshing] = useState(false);
+
+  useEffect(() => {
+    refetch();
+  }, [query]);
+
+  return (
+    <SafeAreaView className="bg-primary h-full">
+      <FlatList
+        data={posts}
+        keyExtractor={(item) => item.$id}
+        renderItem={({ item }) => <VideoCard video={item} />}
+        ListHeaderComponent={() => (
+          <View className="my-6 px-4">
+            <Text className="font-pmedium text-sm text-gray-100">
+              Bookmark Posts
+            </Text>
+            <Text className="text-2xl font-psemibold text-white">{query}</Text>
+
+            <View className='mt-6 mb-8'>
+            <SearchInput initialQuery={query} />
+            </View>
+          </View>
+        )}
+        ListEmptyComponent={() => (
+          <EmptyState
+            title="No Videos Found"
+            subtitle="No videos found for this search query"
+          />
+        )}
+      />
+    </SafeAreaView>
+  );
+};
+
+export default Bookmark;
